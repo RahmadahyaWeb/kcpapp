102907b64ef7933797a6379b5f6b8180ff9aa347
RahmadahyaWeb <thelastpc24@gmail.com>
2024-11-18 15:33:04 +0700

    scheduler sales order

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:test-scheduler')->everyTenSeconds();
+        $schedule->command('app:sync-sales-order')->everyTenSeconds();
     }
 
     /**
