250cb8753d628ffbad105dfb4fd03e23e06dc9a0
RahmadahyaWeb <thelastpc24@gmail.com>
2024-11-25 09:53:25 +0700

    perbaikan data aop

--- resources/views/AOP/final.blade.php
+++ resources/views/AOP/final.blade.php
@@ -4,8 +4,7 @@
 
 @section('breadcrumb')
     <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="{{ route('aop.index') }}">Data Upload AOP</a></li>
-        <li class="breadcrumb-item active"><a href="{{ route('aop.index') }}">Data AOP Final</a></li>
+        <li class="breadcrumb-item active">Data AOP Final</li>
     </ol>
 @endsection
 
