38bbb7fb2cc065417f67cba6dc4aa261433ea57e
RahmadahyaWeb <thelastpc24@gmail.com>
2024-12-02 19:39:53 +0700

    perbaikan

--- resources/views/livewire/dks-table.blade.php
+++ resources/views/livewire/dks-table.blade.php
@@ -28,75 +28,78 @@
             </div>
         </div>
 
-        <div class="table-responsive mb-3">
-            <table class="table table-hover table-sm">
-                <thead class="table-dark">
-                    <tr>
-                        <th>Nama</th>
-                        <th>Tgl. Kunjungan</th>
-                        <th>Toko</th>
-                        <th>Check In</th>
-                        <th>Katalog</th>
-                        <th>Check Out</th>
-                        <th>Lama Kunjungan</th>
-                        <th>Keterangan</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @if ($items->isEmpty())
+        <div class="card-body">
+            <div class="table-responsive mb-3">
+                <table class="table table-hover table-sm">
+                    <thead class="table-dark">
                         <tr>
-                            <td colspan="8" class="text-center">No data</td>
+                            <th>Nama</th>
+                            <th>Tgl. Kunjungan</th>
+                            <th>Toko</th>
+                            <th>Check In</th>
+                            <th>Katalog</th>
+                            <th>Check Out</th>
+                            <th>Lama Kunjungan</th>
+                            <th>Keterangan</th>
                         </tr>
-                    @else
-                        @foreach ($items as $item)
-                            @php
-                                $carbonDate = \Carbon\Carbon::parse($item->tgl_kunjungan);
-                                $formattedDate = $days[$carbonDate->format('l')] . ', ' . $carbonDate->format('d-m-Y');
-                            @endphp
+                    </thead>
+                    <tbody>
+                        @if ($items->isEmpty())
                             <tr>
-                                <td>{{ $item->user_sales }}</td>
-                                <td>{{ $formattedDate }}</td>
-                                <td>{{ $item->nama_toko }}</td>
-                                <td>{{ date('H:i:s', strtotime($item->waktu_cek_in)) }}</td>
-                                <td>
-                                    @if ($item->katalog_at)
-                                        {{ date('H:i:s', strtotime($item->katalog_at)) }}
-                                    @else
-                                        Belum scan katalog
-                                    @endif
-                                </td>
-                                @if (in_array($item->kd_toko, $tokoAbsen))
-                                    <td>
-                                        @if ($item->waktu_cek_out)
-                                            {{ date('H:i:s', strtotime($item->waktu_cek_out)) }}
-                                        @else
-                                            Belum check out
-                                        @endif
-                                    </td>
-                                    <td>-</td>
-                                    <td>Absen Toko</td>
-                                @else
-                                    <td>
-                                        @if ($item->waktu_cek_out)
-                                            {{ date('H:i:s', strtotime($item->waktu_cek_out)) }}
-                                        @else
-                                            Belum check out
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if ($item->lama_kunjungan != null)
-                                            {{ $item->lama_kunjungan }} menit
-                                        @else
-                                            -
-                                        @endif
-                                    </td>
-                                    <td>{{ $item->keterangan }}</td>
-                                @endif
+                                <td colspan="8" class="text-center">No data</td>
                             </tr>
-                        @endforeach
-                    @endif
-                </tbody>
-            </table>
+                        @else
+                            @foreach ($items as $item)
+                                @php
+                                    $carbonDate = \Carbon\Carbon::parse($item->tgl_kunjungan);
+                                    $formattedDate =
+                                        $days[$carbonDate->format('l')] . ', ' . $carbonDate->format('d-m-Y');
+                                @endphp
+                                <tr>
+                                    <td>{{ $item->user_sales }}</td>
+                                    <td>{{ $formattedDate }}</td>
+                                    <td>{{ $item->nama_toko }}</td>
+                                    <td>{{ date('H:i:s', strtotime($item->waktu_cek_in)) }}</td>
+                                    <td>
+                                        @if ($item->katalog_at)
+                                            {{ date('H:i:s', strtotime($item->katalog_at)) }}
+                                        @else
+                                            Belum scan katalog
+                                        @endif
+                                    </td>
+                                    @if (in_array($item->kd_toko, $tokoAbsen))
+                                        <td>
+                                            @if ($item->waktu_cek_out)
+                                                {{ date('H:i:s', strtotime($item->waktu_cek_out)) }}
+                                            @else
+                                                Belum check out
+                                            @endif
+                                        </td>
+                                        <td>-</td>
+                                        <td>Absen Toko</td>
+                                    @else
+                                        <td>
+                                            @if ($item->waktu_cek_out)
+                                                {{ date('H:i:s', strtotime($item->waktu_cek_out)) }}
+                                            @else
+                                                Belum check out
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if ($item->lama_kunjungan != null)
+                                                {{ $item->lama_kunjungan }} menit
+                                            @else
+                                                -
+                                            @endif
+                                        </td>
+                                        <td>{{ $item->keterangan }}</td>
+                                    @endif
+                                </tr>
+                            @endforeach
+                        @endif
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
