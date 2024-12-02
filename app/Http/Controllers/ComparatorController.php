0700396a8f4838ae288a8d83944b4822f1157401
RahmadahyaWeb <thelastpc24@gmail.com>
2024-12-02 15:34:28 +0700

    fitur comparator

--- app/Http/Controllers/ComparatorController.php
+++ app/Http/Controllers/ComparatorController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ComparatorController extends Controller
 {
     public function index()
     {
         return view('comparator.index');
+    }
+
+    public function destroy($part_number)
+    {
+        DB::table('comparator')
+            ->where('part_number', $part_number)
+            ->delete();
+
+        return back()->with('success', 'Data berhasil dihapus');
     }
 }
