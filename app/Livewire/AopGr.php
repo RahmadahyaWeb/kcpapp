d2a10ef879b5b1273abbdaebe0455afdddbfe3f2
rahmadahyaWeb <thelastpc24@gmail.com>
2024-12-11 09:09:28 +0800

    perbaikan

--- app/Livewire/AopGr.php
+++ app/Livewire/AopGr.php
@@ -12,57 +12,18 @@ class AopGr extends Component
     use WithPagination, WithoutUrlPagination;
     protected $paginationTheme = 'bootstrap';
 
+    public $target = 'invoiceAop, spb';
     public $invoiceAop;
     public $spb;
-
-    public function getTotalQty($spb)
-    {
-        return DB::table('invoice_aop_header')
-            ->where('SPB', $spb)
-            ->sum('qty');
-    }
-
-    public function getInvoices($spb)
-    {
-        $invoices = DB::table('invoice_aop_header')
-            ->select(['invoiceAop', 'status'])
-            ->where('SPB', $spb)
-            ->get();
-
-        $invoiceArray = [];
-        foreach ($invoices as $invoice) {
-            $invoiceArray[] = $invoice->invoiceAop;
-        }
-
-        return $invoiceArray;
-    }
-
-    public function getIntransitBySpb($spb)
-    {
-        $intransitStock = DB::connection('kcpinformation')
-            ->table('intransit_header as a')
-            ->join('intransit_details as b', 'a.no_sp_aop', '=', 'b.no_sp_aop')
-            ->where('a.no_sp_aop', '=', $spb)
-            ->select('a.no_sp_aop', 'a.kd_gudang_aop', 'a.tgl_packingsheet', 'b.no_packingsheet', 'b.no_doos', 'b.part_no', 'b.qty', 'b.qty_terima')
-            ->get();
-
-        $totalQtyTerima = 0;
-
-        if ($intransitStock) {
-            foreach ($intransitStock as $item) {
-                $totalQtyTerima += $item->qty_terima;
-            }
-        }
-
-        return $totalQtyTerima;
-    }
 
     public function render()
     {
         $items = DB::table('invoice_aop_header')
             ->select(['*'])
             ->where('status', 'BOSNET')
-            ->get();
+            ->where('invoiceAop', 'like', '%' . $this->invoiceAop . '%')
+            ->where('SPB', 'like', '%' . $this->spb . '%')
+            ->paginate(20);
 
         return view('livewire.aop-gr', compact('items'));
     }
--- resources/views/livewire/aop-gr.blade.php
+++ resources/views/livewire/aop-gr.blade.php
@@ -1,66 +1,60 @@
 <div>
+    <x-alert />
+    <x-loading :target="$target" />
+
     <div class="row mb-3">
         <div class="col">
             <div class="card">
                 <div class="card-header">
                     <b>Data Good Receipt AOP</b>
-                    <hr>
                 </div>
                 <div class="card-body">
                     <div class="row mb-3">
                         <div class="col-md-6 mb-3">
                             <label class="form-label">Invoice AOP</label>
                             <input type="text" class="form-control" wire:model.live.debounce.1000ms="invoiceAop"
-                                placeholder="Invoice AOP" wire:loading.attr="disabled">
+                                placeholder="Invoice AOP">
                         </div>
                         <div class="col-md-6 mb-3">
                             <label class="form-label">SPB</label>
                             <input type="text" class="form-control" wire:model.live.debounce.1000ms="spb"
-                                placeholder="SPB" wire:loading.attr="disabled">
+                                placeholder="SPB">
                         </div>
                     </div>
 
                     <div class="table-responsive">
-                        @if (empty($items))
-                            <table class="table table-hover">
-                                <thead>
+                        <table class="table table-hover">
+                            <thead class="table-dark">
+                                <tr>
+                                    <th>Invoice</th>
+                                    <th>SPB</th>
+                                    <th>Qty</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($items as $item)
                                     <tr>
-                                        <th>SPB</th>
+                                        <td>
+                                            <span style="font-size: 0.9375rem" class="badge p-0">
+                                                <a href="{{ route('aop-gr.detail', $item->invoiceAop) }}">
+                                                    {{ $item->invoiceAop }}
+                                                </a>
+                                            </span>
+                                        </td>
+                                        <td>{{ $item->SPB }}</td>
+                                        <td>{{ $item->qty }}</td>
                                     </tr>
-                                </thead>
-                                <tbody>
+                                @empty
                                     <tr>
-                                        <td colspan="1" class="text-center">No Data</td>
+                                        <td class="text-center" colspan="3">No Data</td>
                                     </tr>
-                                </tbody>
-                            </table>
-                        @else
-                            <table class="table table-hover">
-                                <thead>
-                                    <tr>
-                                        <th>Invoice</th>
-                                        <th>SPB</th>
-                                        <th>Qty</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($items as $item)
-                                        <tr>
-                                            <td>
-                                                <span style="font-size: 0.9375rem" class="badge p-0">
-                                                    <a href="{{ route('aop-gr.detail', $item->invoiceAop) }}">
-                                                        {{ $item->invoiceAop }}
-                                                    </a>
-                                                </span>
-                                            </td>
-                                            <td>{{ $item->SPB }}</td>
-                                            <td>{{ $item->qty }}</td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        @endif
+                                @endforelse
+                            </tbody>
+                        </table>
                     </div>
+                </div>
+                <div class="card-footer">
+                    {{ $items->links() }}
                 </div>
             </div>
         </div>
