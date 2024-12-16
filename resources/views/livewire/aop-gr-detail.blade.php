30075ba9aa2b0908fe1f21213a4b86428bdea3fb
rahmadahyaWeb <thelastpc24@gmail.com>
2024-12-16 14:26:18 +0800

    perbaikan

--- resources/views/livewire/aop-gr-detail.blade.php
+++ resources/views/livewire/aop-gr-detail.blade.php
@@ -49,10 +49,7 @@
                             <tr>
                                 <td>
                                     <input type="checkbox" wire:model.change="selectedItems"
-                                        value="{{ $item->materialNumber }}"
-                                        @if (
-                                            !($item->qty >= $item->qty_terima - ($item->asal_qty ? $item->asal_qty->sum('qty') : 0)) ||
-                                                $item->status == 'BOSNET') disabled @endif />
+                                        value="{{ $item->materialNumber }}" @disabled(!$item->qty >= $item->qty_terima - ($item->asal_qty ? $item->asal_qty->sum('qty') : 0) || $item->status == 'BOSNET') />
                                 </td>
                                 <td>{{ $item->materialNumber }}</td>
                                 <td>{{ $item->qty }}</td>
